0508473b9d1005ddfa27429be02fed64b56b2ae0
Gergely Imreh <imrehg@gmail.com>
2009-09-14 09:52:35 +0800

    wavenet: update py2exe compilation file

--- projects/wavenet/setup.py
+++ projects/wavenet/setup.py
@@ -1,4 +1,19 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['src/wavenetserver.py'])
+Mydata_files = [('', ['src/wavenetserver.cfg'])]
+
+setup(
+    name="WaveNet Server",
+    version="0.1",
+    description="Wavenet: Wavemeter data made accessible",
+    author="Gergely Imreh",
+    console=['src/wavenetserver.py'],
+    data_files=Mydata_files,
+    options={
+                "py2exe":{
+                        "unbuffered": True,
+                        "optimize": 2,
+                }
+        }
+    )
