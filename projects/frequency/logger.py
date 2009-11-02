3c11c2990c4ef4086fcace5229a98d44f66fad56
Gergely Imreh <imrehg@gmail.com>
2009-11-02 09:51:03 +0800

    frequency: prepare logger for both measurements

--- projects/frequency/logger.py
+++ projects/frequency/logger.py
@@ -1,24 +1,38 @@
 from time import time, strftime, sleep
 from numpy import savetxt, array
 from agilentcounter import AgilentCounter
+from agilentmultimeter import AgilentMultimeter
 
-counter = AgilentCounter(gpib=1)
+# Config settings
+countergpib = 1
+multigpib = 6
+vrange = 1
+vresolution = 0.0001
+
+# Setting up frequency counter
+counter = AgilentCounter(gpib = countergpib)
 counter.reset()
 counter.setupFast()
-# Measure frequency on channel 1
 counter.write(":FUNC 'FREQ 1'")
 counter.setupGating(0.1)
 
+# Setting up multimeter
+multi = AgilentMultimeter(gpib = multigpib)
+multi.reset()
+
+# Setting up output file
 datafile = "beat_%s.log" %(strftime("%y%m%d_%H%M%S"))
 out = file(datafile, 'a')
+savetxt(out, "Time(UnixTime) BeatFrequency(Hz) Voltage(V)")
 
 while True:
     try:
         start = time()
         counter.initMeasure()
+        volts = multi.getDCVoltage(vrange,vresolution)
         freq = counter.getFreq()
         now = (time() + start) / 2
-        result = array([[now, freq]])
+        result = array([[now, freq, volts]])
         savetxt(out, result)
         print "%f Hz" %(freq)
     except (KeyboardInterrupt):
