1329602b226195d8ee7938e5542f895c86ef58a8
Gergely Imreh <imrehg@gmail.com>
2009-11-02 11:18:01 +0800

    frequency: add new line to header for correct logging

--- projects/frequency/logger.py
+++ projects/frequency/logger.py
@@ -23,7 +23,7 @@ multi.reset()
 # Setting up output file
 datafile = "beat_%s.log" %(strftime("%y%m%d_%H%M%S"))
 out = file(datafile, 'a')
-out.write("Time(UnixTime) BeatFrequency(Hz) Voltage(V)")
+out.write("Time(UnixTime) BeatFrequency(Hz) Voltage(V)\n")
 
 # Do logging until stopped by Ctrl-C
 while True:
