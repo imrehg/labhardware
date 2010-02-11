d5a22d0376c81998c5503024863cbb6be3e1e922
Gergely Imreh <imrehg@gmail.com>
2010-02-12 00:31:45 +0800

    ionpump: tweak plotting fuction
    + more comments
    + executable
    + time scaling

--- projects/ionpump/plotlog.py
+++ projects/ionpump/plotlog.py
@@ -1,13 +1,21 @@
+#!/usr/bin/env python
+
+# Simple data file plotting
+
 from pylab import *
 from numpy import loadtxt
 
 LOGFILE = 'pump.log'
 data = loadtxt(LOGFILE)
 
-times = (data[:,0] - data[0,0]) / 60
+# Time since start in hours
+times = (data[:,0] - data[0,0]) / 60 / 60
+
+# Ion pump back panel monitor (x10 signal amplification)
+# With amp, 10V = 1mA
 val = data[:,1] / 10 
+
 plot(times, val)
-xlabel("Time (min)")
+xlabel("Time (h)")
 ylabel("Ion pump current (mA)")
 show()
-
