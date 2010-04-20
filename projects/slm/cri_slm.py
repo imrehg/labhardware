7c29f068ef683d3c489f2b2e05067e07fd17bf34
Gergely Imreh <imrehg@gmail.com>
2010-04-20 15:21:43 +0800

    slm: update real driver

--- projects/slm/cri_slm.py
+++ projects/slm/cri_slm.py
@@ -19,7 +19,10 @@ class SLM:
                                    stopbits=1, parity=serial.PARITY_NONE, \
                                    timeout=10, xonxoff=0, rtscts=0, dsrdtr=0)
         self.termchar = "\r"
-
+        self._NMask = 2
+        self._NFrame = 128
+        self._NElement = 128
+        self._MaxValue = 4096
 
     def set(self, command):
         """ Send command to controller, and read answer if there's any
@@ -33,15 +36,68 @@ class SLM:
         resp = self.iface.readline(eol=self.termchar).strip()
         return resp
 
-slm = SLM('/dev/ttyUSB0')
+    def cmdmask(self, command):
+        if command == '?':
+            return self.query("M?")
+        else:
+            try:
+                m = int(command)
+            except:
+                return
+            if m in range(self._NMask):
+                self.set("M %d" %m)
+            return
 
-# set all levels to a previously known transmission level
-drive = [820, 0]
-for m in [0, 1]:
-    slm.set("M %d" %(m))
-    for i in xrange(0, 128):
-        slm.set("E %d" %(i))
-        slm.set("D %d" %(drive[m]))
-    queries = ["M?", "E?", "D?"]
-    for q in queries:
-        print slm.query(q)
+    def cmdframe(self, command):
+        if command == '?':
+            return self.query("F?")
+        else:
+            try:
+                f = int(command)
+            except:
+                return
+            if f in range(self._NFrame):
+                self.set("F %d" %f)
+            return
+
+    def cmdelement(self, command):
+        if command == '?':
+            return self.query("E?")
+        else:
+            try:
+                e = int(command)
+            except:
+                return
+            if e in range(self._NElement):
+                self.set("E %d" %e)
+            return
+
+    def cmdvalue(self, command):
+        if command == '?':
+            return self.query("D?")
+        else:
+            try:
+                v = int(command)
+            except:
+                return
+            if 0 <= v < self._MaxValue:
+                self.set("D %d" %v)
+            return
+
+    def maxmask(self):
+        return self._NMask
+
+    def maxframe(self):
+        return self._NFrame
+
+    def maxelement(self):
+        return self._NElement
+
+    def maxvalue(self):
+        return self._MaxValue
+
+    def clearframe(self):
+        self.set("C 1")
+                
+    def activeframe(self, frame):
+        self.set("P %d" frame)
