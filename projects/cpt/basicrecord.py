61a763e0f3476b14b1c9efd9bc2611f11e0b9714
Gergely Imreh <imrehg@gmail.com>
2010-05-19 11:16:19 +0800

    cpt: move settings into configuration file

--- projects/cpt/basicrecord.py
+++ projects/cpt/basicrecord.py
@@ -1,42 +1,72 @@
 from __future__ import division
 
-from time import sleep
+from time import sleep, strftime
 from numpy import *
+import ConfigParser
+import sys
+import logging
+ 
 import agilent8644
 import stanfordSR830
 
-synth = agilent8644.Agilent8644(7)
-lockin = stanfordSR830.StanfordSR830(8)
+try:
+    configfile = sys.argv[1]
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(configfile))
+except:
+    print "Cannot find configuration file."
+    sys.exit(1)
 
-clock =  9192631770
-clockscan = [-5, 5]
+# Import configuration and do basic setup
+synth = agilent8644.Agilent8644(config.getint('Setup','synth_GPIB'))
+lockin = stanfordSR830.StanfordSR830(config.getint('Setup','lockin_GPIB'))
+synthdivider = config.getint('Setup','synthdivider')
 
-synthmulti = 20
+clock =  config.getfloat('Setup','clock')
+clockscan = [config.getfloat('Experiment','scanstart'),
+             config.getfloat('Experiment','scanstop')]
+scansteps = config.getint('Experiment','scansteps')
+repeats = config.getint('Experiment','repeats')
+lockinrate = config.getint('Experiment','lockinrate')
 
-scansteps = 5
-repeats = 5
+logger = logging.getLogger()
+logfile = config.get('Setup','logfile')
+if logfile == 'auto':
+    logfile = "cpt_%s.log" %(strftime("%y%m%d_%H%M%S"))
+hdlr = logging.FileHandler(logfile)
+formatter = logging.Formatter('%(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO) 
 
-synth.write("FREQ:CW %f HZ" %(clock/synthmulti))
+# Save configuration info
+f = open(configfile)
+for line in f:
+    logger.info("# %s" %line.strip())
+f.close()
+
+synth.write("FREQ:CW %f HZ" %(clock/synthdivider))
 
 q = ["FREQ:CW?"]
 for quest in q:
     print quest, "->", synth.ask(quest)
 
 lockin.write("REST")
-lockin.write("SRAT 8")
+lockin.write("SRAT %d" %(lockinrate))
 lockin.write("FAST 0")
 q = ["SRAT?", "SPTS?", "SEND?"]
 for quest in q:
     print quest, "->", lockin.ask(quest)
 
 print ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
+logging.info("#AimedClockFrequency(Hz) LockInSignal(V)")
 
 ss = linspace(clockscan[0],clockscan[1],scansteps)
 results = zeros((scansteps, 2))
 for index, scanning in enumerate(ss):
     freq = clock + scanning
     print "Detuning %d / %d: %f Hz" %(index+1, scansteps, scanning) 
-    synth.write("FREQ:CW %f HZ" %(freq/synthmulti))
+    synth.write("FREQ:CW %f HZ" %(freq/synthdivider))
     # Let it settle
     sleep(1)
     lockin.write("REST")
@@ -48,5 +78,5 @@ for index, scanning in enumerate(ss):
     temp = lockin.ask("TRCA?1,0,%d" %(repeats))
     temp2 = array([float(x) for x in temp.split(',')[0:-1]])
     results[index, 0:2] = [freq, average(temp2)]
-
-savetxt('test.out',results)
+    for measure in temp2:
+        logger.info("%1.3f,%e" %(freq, measure))
