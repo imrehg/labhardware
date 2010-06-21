9d09b39e0224c1d59853835040411b1f382eecbd
unknown <Lab@.(none)>
2010-06-21 16:59:57 +0800

    pd_daq: update program to use config file

--- projects/pd_daq/cont_log.py
+++ projects/pd_daq/cont_log.py
@@ -2,6 +2,23 @@
 from nidaqmx import *
 from time import strftime
 import logging
+import ConfigParser
+import sys
+from time import time
+
+try:
+    configfile = sys.argv[1]
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(configfile))
+except:
+    print "Cannot find configuration file."
+    sys.exit(1)
+
+# Load settings
+device = config.get('Setup', 'deviceID')
+channel = config.get('Setup', 'channelID')
+readrate = config.getfloat('Experiment', 'readrate')
+saverate = config.getint('Experiment', 'saverate')
 
 # Setup logging
 logger = logging.getLogger()
@@ -15,18 +32,30 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO) 
 
+# Save configuration info
+f = open(configfile)
+for line in f:
+    logger.info("# %s" %line.strip())
+f.close()
+
+# Set up logging
 task = AnalogInputTask()
-task.create_voltage_channel('Dev1/ai0', terminal = 'rse', min_val=0, max_val=10.0)
-task.configure_timing_sample_clock(rate = 500.0, sample_mode='continuous')
+task.create_voltage_channel('%s/%s' %(device, channel),
+                            terminal = 'diff',
+                            min_val=0,
+                            max_val=10.0)
+task.configure_timing_sample_clock(rate = readrate,
+                                   sample_mode='continuous')
 
 task.start()
 
+start = time()
 while True:
     try:
-        data = task.read(2000)
+        data = task.read(saverate)
         for d in data:
             logger.info(d[0])
-        print "Written!"
+        print "%.0fs :: Written!" %(time()-start)
     except KeyboardInterrupt:
         del task
         print "Finished"
