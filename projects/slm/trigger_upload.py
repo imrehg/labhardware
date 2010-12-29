088547eff77254578d04610c3cd5d6cc93d5ce3f
Gergely Imreh <imrehg@gmail.com>
2010-12-29 18:29:29 +0800

    slm: trigger_upload - basic functionality version
    tested only with the fake driver (there seem to work)
    includes lots of todo items as well (due "when there'll be time")

---
+++ projects/slm/trigger_upload.py
@@ -0,0 +1,60 @@
+"""
+Uploading triggerable data to the spatial light modulator
+
+TODO:
++ How to treat the two masks?
++ Error handling
++ Generalization: make it importable or use import from other slmcontrol?
++ Confirmation before upload
++ Show in file-selection title the index of the file to be uploaded ("select file #3")
++ Store settings in conf
++ Automatic port selection
+"""
+
+#!/usr/bin/env python
+import sys
+
+import ConfigParser
+import os
+import numpy as np
+
+#import cri_slm_fake as cri_slm
+import cri_slm
+import ourgui
+
+if __name__ == "__main__":
+    dirname = os.path.dirname(sys.argv[0])
+    slm = cri_slm.SLM('COM8')
+
+    try:
+        config = ConfigParser.RawConfigParser()
+        config.read(dirname+'/slm.conf')
+    except:
+        print "Error when parsing/applying settings. Continue anyway."
+        pass
+
+    inputFiles = []
+    while True:
+        filename = ourgui.openFile()
+        if filename == "":
+            break
+        else:
+            inputFiles += [filename]
+
+    ninput = len(inputFiles)
+    print("Selected %d files" %(ninput))
+    if ninput == 0:
+        sys.exit(1)
+
+    # Load data files
+    data = np.zeros((128, ninput))
+    for i in xrange(ninput):
+        temp = np.loadtxt(inputFiles[i])
+        data[:,i] = temp
+
+    # Upload data frame-by-frame
+    for i in xrange(128):
+        slm.activeframe(i);
+        slm.blockset(data[:, i % ninput])
+
+    print "Done"
