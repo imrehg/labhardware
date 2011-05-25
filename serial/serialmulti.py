96e9e34a92b068fb61c5b77a951fada943eae5e9
Gergely Imreh <imrehg@gmail.com>
2011-05-25 10:25:49 +0800

    serial multimeter: make it into an object

--- serial/serialmulti.py
+++ serial/serialmulti.py
@@ -11,51 +11,60 @@
 
 import serial
 import re
+from numpy import NaN
 
-def interpret(line):
-    mode = line[0:2]
-    value = line[2:9].strip()
-    unit = line[9:-1].strip()
+class Multimeter:
 
-    if  (re.match('O.L',value)):
-        value = 'inf'
- 
-    if (mode == 'OH'):
-        print "Resistance: %s %s" % (value, unit)
-    elif (mode == 'DC'):
-        if (re.search('V',unit)):
-            print "DC Voltage: %s %s" % (value, unit)
-        if (re.search('A',unit)):
-            print "DC Current: %s %s" % (value, unit)
-    elif (mode == 'AC'):
-        if (re.search('V',unit)):
-            print "AC Voltage: %s %s" % (value, unit)
-        if (re.search('A',unit)):
-            print "AC Current: %s %s" % (value, unit)
+    def __init__(self):
+        """ Setting up connection """
+        self.ser = serial.Serial('/dev/ttyUSB0',baudrate=600, bytesize=7, stopbits=2, \
+                 parity=serial.PARITY_NONE, timeout=1, xonxoff=1, rtscts=0, dsrdtr=1)
+        # Needs the many DTR/RTS things for some reason
+        self.ser.setDTR(1)
+        self.ser.setRTS(0)
+        self.ser.setDTR(1)
+        self.ser.setRTS(0)
+        self.ser.setDTR(1)
+        self.ser.setRTS(0)
+        self.ser.setDTR(1)
+        self.ser.setRTS(0)
+        self.pattern = re.compile(r".*(?P<mode>OH)\s*(?P<value>[\d.]*)\s*(?P<unit>[a-zA-Z]*)")
 
+        
+    def interpret(self, mode, value, unit):
+        if  (re.match('O.L',value)):
+            value = NaN
 
-# Setting up connection. Needs the many DTR/RTS things for some reason....
-ser = serial.Serial('/dev/ttyUSB0',baudrate=600, bytesize=7, stopbits=2, \
-    parity=serial.PARITY_NONE, timeout=1, xonxoff=1, rtscts=0, dsrdtr=1)
-ser.setDTR(1)
-ser.setRTS(0)
-ser.setDTR(1)
-ser.setRTS(0)
-ser.setDTR(1)
-ser.setRTS(0)
-ser.setDTR(1)
-ser.setRTS(0)
-try:
-    while True:
-        ser.write("D")
-        line = ser.readline()
-        if ( re.match('^[ODA]+', line) ):
-            #~ print "%s" % (line)
-            interpret(line)
-except (KeyboardInterrupt):
-    # Exit with Control-C
-    pass
-except:
-    raise
-finally:
-    ser.close()
+        value = float(value)
+        if (mode == 'OH'):
+            if unit == "kOhm":
+                value *= 1000
+            elif unit == "MOhm":
+                valiue *= 1000000
+            unit = "Ohm"
+
+        return (value, unit)
+
+        # elif (mode == 'DC'):
+        #     if (re.search('V',unit)):
+        #         print "DC Voltage: %s %s" % (value, unit)
+        #     if (re.search('A',unit)):
+        #         print "DC Current: %s %s" % (value, unit)
+        # elif (mode == 'AC'):
+        #     if (re.search('V',unit)):
+        #         print "AC Voltage: %s %s" % (value, unit)
+        #     if (re.search('A',unit)):
+        #         print "AC Current: %s %s" % (value, unit)
+
+    def ask(self):
+        self.ser.write("D")
+        line = self.ser.readline()
+        data = self.pattern.match(line)
+        if data:
+            result = self.interpret(data.group('mode'), data.group('value'), data.group('unit'))
+        else:
+            result = None
+        return result
+
+    def close(self):
+        self.ser.close()
