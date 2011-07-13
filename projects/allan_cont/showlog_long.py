085f242eb7a56ad2dff75f6716a2f31c29eb5026
Gergely Imreh <imrehg@gmail.com>
2011-07-13 16:05:06 +0800

    allan: quick plotting of long log histogram and timeseries

---
+++ projects/allan_cont/showlog_long.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pylab as pl
+from ourgui import openFile
+
+def plotline(maxx, minx=0, value=0, style="k-", plotfunc=pl.plot):
+    plotfunc([minx, maxx], [value, value], style)
+
+def quickplot(filename):
+    data = np.loadtxt(filename, comments="#")
+    maxdata, mindata, stddata, meandata = np.max(data), np.min(data), np.std(data), np.mean(data)
+
+    n = len(data)
+    pl.subplot(211)
+    pl.plot(data,'k.')
+    plotline(n, value=maxdata, style="g-")
+    plotline(n, value=mindata, style="r-")
+    plotline(n, value=meandata, style="k-")
+    plotline(n, value=(meandata+stddata), style="b-")
+    plotline(n, value=(meandata-stddata), style="b-")
+
+    pl.xlabel('data points')
+    pl.ylabel('Frequency (Hz)')
+    pl.title("Frequency: %f (+- %f) Hz" %(meandata, stddata))
+
+    pl.subplot(212)
+    n, bins, patches = pl.hist(data-meandata, 100, normed=1, facecolor='green', alpha=0.75)
+    pl.xlabel('Frequency deviation from mean (Hz)')
+    pl.ylabel('distribution')
+
+    pl.show()
+
+filename = openFile("log")
+if filename:
+    quickplot(filename)
