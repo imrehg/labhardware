ac084c574b58771bd240af3fa4b4a000fc742229
Gergely Imreh <imrehg@gmail.com>
2011-07-18 19:04:12 +0800

    slowlog_long: update to handle different kinds of files

--- projects/allan_cont/showlog_long.py
+++ projects/allan_cont/showlog_long.py
@@ -6,7 +6,14 @@ def plotline(maxx, minx=0, value=0, style="k-", plotfunc=pl.plot):
     plotfunc([minx, maxx], [value, value], style)
 
 def quickplot(filename):
-    data = np.loadtxt(filename, comments="#")
+    alldata = np.loadtxt(filename, comments="#", delimiter=",")
+    datashape = np.shape(alldata)
+    try:
+        col = np.shape(alldata)[1]
+        data = alldata[:, col-1]
+    except (IndexError):
+        data = alldata
+
     maxdata, mindata, stddata, meandata = np.max(data), np.min(data), np.std(data), np.mean(data)
 
     n = len(data)
