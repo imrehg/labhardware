e89cbd98760580087bf9d7a5f64475ef44f26335
Gergely Imreh <imrehg@gmail.com>
2011-07-23 18:03:28 +0800

    driver:GPS M12M, initial driver
    This basic driver is just a proof of concept for communication,
    implementing a simple version of the XOR binary checksumming
    needed to talk to the chip, and some exaple code to get the
    IDs of the currently followed satellites.

---
+++ drivers/gpsm12m.py
@@ -0,0 +1,37 @@
+import serial
+
+class GPS:
+
+    def __init__(self, com):
+        self.__ser = serial.Serial('/dev/ttyUSB0',
+                                   baudrate=9600,
+                                   bytesize=8, stopbits=1,
+                                   parity=serial.PARITY_NONE,
+                                   timeout=1,
+                                   )
+
+
+    def checksum(self, msg):
+        chk = ord(msg[0]) ^ ord(msg[1])
+        for i in xrange(2, len(msg)):
+            chk = chk ^ ord(msg[i])
+        return chr(chk)
+
+    def ask(self, query):
+        message = "@@%s%s\r\n" %(query, self.checksum(query))
+        print "Query: %s, (%d)" %(query, len(message))
+        self.__ser.write(message)
+        return self.__ser.readline()+self.__ser.readline()
+
+if __name__ == "__main__":
+    gps = GPS(0)
+    # line = gps.ask("Hn0")
+    line = gps.ask("Bb0")
+    resp = line[2:4]
+    print len(line)
+    if resp == "Bb":
+        numsat = ord(line[4])
+        s = 5
+        for i in xrange(numsat):
+            print ord(line[s])
+            s += 7
