7e00fc65a4136512b0f594ba33cb3210e05cf52f
Gergely Imreh <imrehg@gmail.com>
2011-07-28 17:44:07 +0800

    Rubidium clock driver

---
+++ drivers/rbclock.py
@@ -0,0 +1,39 @@
+"""
+Driver for the Stanford Research PRS10 Rubidium Clock
+"""
+import serial
+
+class Clock:
+
+    def __init__(self, COM=0):
+        comport = '/dev/ttyUSB%d' %(COM)  # This is Linux specific like this
+        # Settings from the manual
+        self.__ser = serial.Serial(comport,
+                                   baudrate=9600,
+                                   bytesize=8,
+                                   stopbits=2,
+                                   parity=serial.PARITY_NONE,
+                                   xonxoff=1,
+                                   timeout=1,
+                                   )
+
+    def write(self, message):
+        """ Write to the device """
+        self.__ser.write("%s\r" %message) # Adding the line-feed that is the command delimiter
+
+    def read(self):
+        """ Read one line from the device """
+        return self.__ser.readline()
+
+    def ask(self, query):
+        self.write(query)
+        return self.read()
+
+    def setupWithGPS(self):
+        """ Reset settings suitable for 1PPS stabilization from GPS """
+        clock.write("LM1") # Enable digital pre-filter for the incoming 1PPS signal from GPS
+        clock.write("PT14") # Set longest PLL parameter for 1PPS input with bad short-term stability
+
+if __name__ == "__main__":
+    clock = Clock()
+    print "Unit ID: %s" %(clock.ask("ID?"))
