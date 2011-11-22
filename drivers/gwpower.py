0207b6fd3bd737cf4486e8db5b9060fd82f0a217
Gergely Imreh <imrehg@gmail.com>
2011-11-22 15:48:01 +0800

    gwinstek: managed to read from device

--- drivers/gwpower.py
+++ drivers/gwpower.py
@@ -26,13 +26,15 @@ class PowerMeter:
                                          bytesize=8,
                                          stopbits=1,
                                          parity=serial.PARITY_NONE,
-                                         timeout=1)
+                                         timeout=5,
+                                         xonxoff=1)
                 self.port = i
                 # Required, from snooping
                 self.ser.setRTS(0)
                 self.ser.setRTS(0)
                 self.ser.setDTR(1)
                 self.ser.setDTR(1)
+                self.read(1) # needs to read and throw away this one byte
                 break
             except serial.SerialException:
                 self.ser = None
@@ -44,12 +46,28 @@ class PowerMeter:
             print "Powermeter connected on %s%d" %(portbase, self.port)
 
 
-    def read(self):
-        reply = self.ser.read(14) # every data should be 14 bits
-        print len(reply)
+    def read(self, bytes=14):
+        reply = self.ser.read(bytes) # every data should be 14 bits
+        # # Sometimes it's losing some bytes, this synchronization does not work
+        # if len(reply) < bytes:
+        #     print "underrun... continue"
+        #     reply += self.read(bytes - len(reply))
         return reply
+
+    def getnumber(self, data=None):
+        if data is None:
+            data = self.read()
+
+        toprint = ""
+        for c in data:
+            toprint += "%x " %ord(c)
+        return toprint
 
 if __name__ == "__main__":
 
-    pm = PowerMeter()
-    print pm.read()
+    pm = PowerMeter(3)
+    # print pm.read()
+    from time import sleep
+    while True:
+        print pm.getnumber()
+        sleep(0.1)
