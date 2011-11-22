c7781d811d7c1a24dbc62f7de7337edc8880d06f
Gergely Imreh <imrehg@gmail.com>
2011-11-22 17:13:44 +0800

    beamprofile: working image acquisition and display

--- projects/beamprofile/beamprofile.py
+++ projects/beamprofile/beamprofile.py
@@ -1,10 +1,16 @@
 import pydc1394 as fw
 from time import sleep
+import numpy as np
+import pylab as pl
+
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FixedLocator, FormatStrFormatter
+import matplotlib, time
 
 if __name__ == "__main__":
     l = fw.DC1394Library()
     cams = l.enumerate_cameras()
-    cam0 = fw.Camera(l, cams[0]['guid'])
+    cam0 = fw.Camera(l, cams[0]['guid'], isospeed=800)
 
     print "Connected to: %s / %s" %(cam0.vendor, cam0.model)
 
@@ -12,9 +18,29 @@ if __name__ == "__main__":
     for feat in cam0.features:
         print "%s : %s" %(feat, cam0.__getattribute__(feat).val)
 
-    cam0.start()
-    sleep(0.5)
-    matrix = cam0.shot()     ## Commenting out these
-    print matrix.shape       ## two lines it still hangs....
+    print cam0.modes
+    cam0.mode = "640x480_Y8"  # the Y16 mode does not seem to work
+    print cam0.mode
+
+
+    matplotlib.interactive(True)
+    fig = pl.figure()
+    ax = fig.add_subplot(111)
+
+    cam0.start(interactive=True)
+    imgnum = 0
+    image = None
+    while True:
+        try:
+            # sleep(0.05)
+            if image is not None:
+                image.remove()
+            image = ax.imshow(cam0.current_image)
+            pl.draw()
+            imgnum += 1
+            print "Image #%d" %(imgnum)
+        except KeyboardInterrupt:
+            print "Stopping"
+            break
+
     cam0.stop()
-    print "Done?"
