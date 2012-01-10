22ae013a11b74d3b1f881cea3518fc221f49d9e7
Gergely Imreh <imrehg@gmail.com>
2012-01-10 15:56:09 +0800

    beamprofiler: update to interface

--- projects/beamprofile/beamprofile.py
+++ projects/beamprofile/beamprofile.py
@@ -9,6 +9,7 @@ from matplotlib.ticker import LinearLocator, FixedLocator, FormatStrFormatter
 import matplotlib
 
 import fastfit
+import interface
 
 def sizetext(sx, sy):
     """
@@ -64,59 +65,17 @@ if __name__ == "__main__":
     ax = fig.add_subplot(111)
 
     cam0.start(interactive=True)
-    image = None
+    elements = None
     dimx, dimy = 640, 480
     pixelsize = 5.6
 
     while True:  # image collection and display
         try:
             data = cam0.current_image
-            text = "Data range: %d - %d" %(np.min(data), np.max(data))
-            xx, yy, dx, dy, angle = fastfit.d4s(data)
-            adeg = "%.1f deg" %(-angle / np.pi * 180)
-            xr, yr = fastfit.getellipse(xx, yy, dx, dy, angle)
-            xxx = [xx - dx/2*np.cos(angle), xx + dx/2*np.cos(angle)]
-            xxy = [yy + dx/2*np.sin(angle), yy - dx/2*np.sin(angle)]
-            yyx = [xx + dy/2*np.sin(angle), xx - dy/2*np.sin(angle)]
-            yyy = [yy + dy/2*np.cos(angle), yy - dy/2*np.cos(angle)]
-
-            st = sizetext(dx/2*pixelsize, dy/2*pixelsize)
-
-            if image is None:  # First display, set up output screen
-                # the data
-                image = ax.imshow(data, vmin=0, vmax=255, cmap='Paired')
-
-                # extra display: centre marker, D4s ellipse, axes
-                centre, = ax.plot(xx, yy, '+', markersize=10)
-                ellipse, = ax.plot(xr, yr, 'k-', linewidth=3)
-                ax1, ax2, = ax.plot(xxx, xxy, 'k-', yyx, yyy, 'k-', linewidth=2)
-
-                # reposition picture, since plot ruins imshow's limits
-                centre.axes.set_xlim([0, dimx])
-                centre.axes.set_ylim([dimy-1, 0])
-
-                # Data headers
-                sztext = fig.text(0.5, 0.965, 'wx/wy = Gaussian beam width (um) along principal axes (D4s/2*pixelsize)', horizontalalignment='center', fontsize=20)
-                sztext = fig.text(0.5, 0.81, st, horizontalalignment='center', fontsize=67)
-                atext = fig.text(0.5, 0.10, adeg, horizontalalignment='center', fontsize=65)
-                uptext = fig.text(0.5, 0.05, text, horizontalalignment='center', fontsize=25)
-                # np.save('test', data)
+            if elements is None:  # First display, set up output screen
+                elements = interface.createiface(data)
             else:  # Every other iteration just update data
-                image.set_data(data)
-
-                centre.set_xdata(xx)
-                centre.set_ydata(yy)
-
-                ellipse.set_xdata(xr)
-                ellipse.set_ydata(yr)
-                ax1.set_xdata(xxx)
-                ax1.set_ydata(xxy)
-                ax2.set_xdata(yyx)
-                ax2.set_ydata(yyy)
-
-                uptext.set_text(text)
-                sztext.set_text(st)
-                atext.set_text(adeg)
+                interface.updateiface(data, elements)
             pl.draw()
         except KeyboardInterrupt:
             print "Stopping"
